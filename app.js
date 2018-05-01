591907cc8c5fc0863f6bf0be7450d1853e4699fb
Mo Mesgin <mo.mesgin@gmail.com>
2018-04-30 22:33:58 -0700

    add GoogleStrategy

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const port = process.env.PORT || 5000
+const passport = require('passport')
+
+// Passport config
+require('./config/passport')(passport)
+
+// Load Routes
+const auth = require('./routes/auth')
 
 app.get('/',(req,res)=>{
   res.send('worked')
 })
+
+app.use('/auth',auth)
 
 app.listen(port,()=>{
   console.log(`Server running at ${port}`)
