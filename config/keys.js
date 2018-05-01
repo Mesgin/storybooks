c6e6eaa5658563b8c8561fc2ca01298b306ce680
Mo Mesgin <mo.mesgin@gmail.com>
2018-04-30 22:06:54 -0700

    add google and mongoose keys

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mogoURI: 'mongodb://mo:incognito@ds263639.mlab.com:63639/storybooks-project-db',
+  googleClientID: '89934693842-5mc9gpm6p3br07qo771caqmrqblibvbq.apps.googleusercontent.com',
+  googleClientSecret: '7WDq8CJwhYXslkrybVCq4_HR'
+}
